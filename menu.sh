f951fb0c6ce0a5487042d52c6c87394d52fcd22e
RazVpn <102897626+RazVpn@users.noreply.github.com>
2022-08-12 16:16:39 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -27,17 +27,17 @@ uptime="$(uptime -p | cut -d " " -f 2-10)"
 
 # USERNAME
 rm -f /usr/bin/user
-username=$( curl https://raw.githubusercontent.com/${GitUser}/ipv2/main/ip.conf | grep $MYIP | awk '{print $2}' )
+username=$( curl https://raw.githubusercontent.com/${GitUser}/ipv3/main/ipvps.conf | grep $MYIP | awk '{print $2}' )
 echo "$username" > /usr/bin/user
 
 # Order ID
 rm -f /usr/bin/ver
-user=$( curl https://raw.githubusercontent.com/${GitUser}/ipv2/main/ip.conf | grep $MYIP | awk '{print $3}' )
+user=$( curl https://raw.githubusercontent.com/${GitUser}/ipv3/main/ipvps.conf | grep $MYIP | awk '{print $3}' )
 echo "$user" > /usr/bin/ver
 
 # validity
 rm -f /usr/bin/e
-valid=$( curl https://raw.githubusercontent.com/${GitUser}/ipv2/main/ip.conf | grep $MYIP | awk '{print $4}' )
+valid=$( curl https://raw.githubusercontent.com/${GitUser}/ipv3/main/ipvps.conf | grep $MYIP | awk '{print $4}' )
 echo "$valid" > /usr/bin/e
 
 # DETAIL ORDER
@@ -52,7 +52,7 @@ Info="${Green_font_prefix}(Active)${Font_color_suffix}"
 Error="${Green_font_prefix}${Font_color_suffix}${Red_font_prefix}[EXPIRED]${Font_color_suffix}"
 
 today=`date -d "0 days" +"%Y-%m-%d"`
-Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/ipv2/main/ip.conf | grep $MYIP | awk '{print $4}')
+Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/ipv3/main/ipvps.conf | grep $MYIP | awk '{print $4}')
 if [[ $today < $Exp1 ]]; then
 sts="${Info}"
 else
@@ -119,16 +119,16 @@ echo -e "  \e[$text Provided By          : $creditt"
 echo -e "\e[${line}════════════════════════════════════════════════════════════\e[m" 
 echo -e "\e[$back_text                        \e[30m[\e[$box TOTAL USER\e[30m ]\e[1m                       \e[m"
 echo -e   " \e[$line════════════════════════════════════════════════════════════\e[m"
-echo -e "\e[${text}          XRAY                   TROJAN\e[m" 
-echo -e "\e[${text}      $total_xray                $total_TROJAN\e[m"
+echo -e "   \e[${text}          XRAY                       TROJAN\e[m" 
+echo -e "    \e[${text}      $total_xray                $total_TROJAN\e[m"
 echo -e   " \e[$line════════════════════════════════════════════════════════════\e[m"
 echo -e   " \e[$back_text                        \e[30m[\e[$box MAIN MENU\e[30m ]\e[1m                       \e[m"
 echo -e   " \e[$line════════════════════════════════════════════════════════════\e[m"
 echo -e   "  \e[$number [•1]\e[m \e[$below MENU XRAY VMESS & VLESS\e[m     \e[$number [•6]\e[m \e[$below CHECK RUNNING\e[m"
 echo -e   "  \e[$number [•2]\e[m \e[$below MENU TROJAN XRAY & GO\e[m       \e[$number [•7]\e[m \e[$below CLEAR LOG VPS\e[m"
-echo -e   "  \e[$number [•3]\e[m \e[$below MENU SYSTEM\e[m      \e[$number [•8]\e[m \e[$below CHANGE PORT\e[m"
-echo -e   "  \e[$number [•4]\e[m \e[$below MENU UPDATE\e[m                   \e[$number [•9]\e[m \e[$below INFO ALL PORT\e[m" 
-echo -e   "  \e[$number [•5]\e[m \e[$below MENU THEMES\e[m     \e[$number [10]\e[m \e[$below REBOOT VPS\e[m"
+echo -e   "  \e[$number [•3]\e[m \e[$below MENU SYSTEM\e[m                 \e[$number [•8]\e[m \e[$below CHANGE PORT\e[m"
+echo -e   "  \e[$number [•4]\e[m \e[$below MENU UPDATE\e[m                 \e[$number [•9]\e[m \e[$below INFO ALL PORT\e[m" 
+echo -e   "  \e[$number [•5]\e[m \e[$below MENU THEMES\e[m                 \e[$number [10]\e[m \e[$below REBOOT VPS\e[m"
 echo -e   " \e[$line════════════════════════════════════════════════════════════\e[m"
 echo -e   "  \e[$below Order ID          : $oid"
 echo -e   "  \e[$below Client Name       : $username"
