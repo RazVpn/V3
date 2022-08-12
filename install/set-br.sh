d5bc510c656513c6b64e67b5cb916f255287d7fb
RazVpn <102897626+RazVpn@users.noreply.github.com>
2022-08-12 16:50:09 +0800

    Update set-br.sh

--- install/set-br.sh
+++ install/set-br.sh
@@ -3,7 +3,7 @@
 GitUser="RazVpn"
 curl https://rclone.org/install.sh | bash
 printf "q\n" | rclone config
-wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/${GitUser}/v2/main/rclone.conf"
+wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/${GitUser}/V3/main/rclone.conf"
 git clone  https://github.com/${GitUser}/wondershaper.git
 cd wondershaper
 make install
@@ -28,12 +28,12 @@ logfile ~/.msmtp.log
 EOF
 chown -R www-data:www-data /etc/msmtprc
 cd /usr/bin
-wget -O autobackup "https://raw.githubusercontent.com/${GitUser}/v2/main/system/autobackup.sh"
-wget -O backup "https://raw.githubusercontent.com/${GitUser}/v2/main/system/backup.sh"
-wget -O bckp "https://raw.githubusercontent.com/${GitUser}/v2/main/system/bckp.sh"
-wget -O restore "https://raw.githubusercontent.com/${GitUser}/v2/main/system/restore.sh"
-wget -O strt "https://raw.githubusercontent.com/${GitUser}/v2/main/system/strt.sh"
-wget -O limit-speed "https://raw.githubusercontent.com/${GitUser}/v2/main/limit-speed.sh"
+wget -O autobackup "https://raw.githubusercontent.com/${GitUser}/V3/main/system/autobackup.sh"
+wget -O backup "https://raw.githubusercontent.com/${GitUser}/V3/main/system/backup.sh"
+wget -O bckp "https://raw.githubusercontent.com/${GitUser}/V3/main/system/bckp.sh"
+wget -O restore "https://raw.githubusercontent.com/${GitUser}/V3/main/system/restore.sh"
+wget -O strt "https://raw.githubusercontent.com/${GitUser}/V3/main/system/strt.sh"
+wget -O limit-speed "https://raw.githubusercontent.com/${GitUser}/V3/main/limit-speed.sh"
 chmod +x autobackup
 chmod +x backup
 chmod +x bckp
