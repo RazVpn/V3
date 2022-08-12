c5f35fab7c3b11c947f302b6591db6551e9be9ed
RazVpn <102897626+RazVpn@users.noreply.github.com>
2022-08-12 15:56:39 +0800

    Add files via upload

---
+++ update/update.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+#Script Auto Reboot Vps
+#wget https://github.com/${GitUser}/
+GitUser="RazVpn"
+
+#IZIN SCRIPT
+MYIP=$(curl -sS ipv4.icanhazip.com)
+
+# Valid Script
+VALIDITY () {
+    today=`date -d "0 days" +"%Y-%m-%d"`
+    Exp1=$(curl https://raw.githubusercontent.com/RazVpn/ipv2/main/ip.conf | grep $MYIP | awk '{print $4}')
+    if [[ $today < $Exp1 ]]; then
+    echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
+    else
+    echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m";
+    echo -e "\e[31mPlease renew your ipvps first\e[0m"
+    exit 0
+fi
+}
+IZIN=$(curl https://raw.githubusercontent.com/RazVpn/ipv2/main/ip.conf | awk '{print $5}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo -e "\e[32mPermission Accepted...\e[0m"
+VALIDITY
+else
+echo -e "\e[31mPermission Denied!\e[0m";
+echo -e "\e[31mPlease buy script first\e[0m"
+exit 0
+fi
+clear
+RED='\e[1;31m'
+GREEN='\e[0;32m'
+BLUE='\e[0;34m'
+NC='\e[0m'
+ver=$(cat /home/ver)
+ver=$( curl https://raw.githubusercontent.com/${GitUser}/versionv2/main/ver.conf )
+clear
+
+# CEK UPDATE
+Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
+Info1="${Green_font_prefix}[$ver]${Font_color_suffix}"
+Info2="${Green_font_prefix}[LATEST VERSION]${Font_color_suffix}"
+Error="Version ${Green_font_prefix}[$ver]${Font_color_suffix} available${Red_font_prefix}[Please Update]${Font_color_suffix}"
+ver=$(cat /home/ver)
+new_version=$( curl https://raw.githubusercontent.com/${GitUser}/versionv2/main/ver.conf | grep $ver )
+#Status Version
+if [ $ver = $new_version ]; then
+sts="${Info2}"
+else
+sts="${Error}"
+fi
+clear
+
+# Echo Shell
+echo ""
+echo ""
+echo -e "\e[0;36m-------------------------------------------------------" 
+echo -e "                    UPDATE SCRIPT MENU                 " 
+echo -e "\e[0;36m-------------------------------------------------------" 
+echo -e " \e[0;36mVERSION NOW \e[0;33m>>\e[0m $Info1"
+echo -e " \e[0;36mSTATUS UPDATE \e[0;33m>>\e[0m $sts"
+echo -e " "
+echo -e "     \e[0;36m1.\e[0m Change Banner SSH"
+echo -e "     \e[0;36m2.\e[0m Check Update/New Version Script"
+echo ""
+echo -e "\e[0;36m--------------------------------------------------------" 
+echo -e "    \e[0;32m[x]\e[0m  Back To Main Menu"
+echo -e ""
+read -p "     Select From Options [1-3 or x] :  " port
+echo -e ""
+case $port in
+1)
+message-ssh
+;;
+2)
+run-update
+;;
+x)
+clear
+menu
+;;
+*)
+clear
+echo -e "\e[1;31mPlease enter an correct number, Try again.\e[0m"
+sleep 2
+update
+;;
+esac
